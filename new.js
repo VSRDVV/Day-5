a0588f5135df0cf9f50a557bd3291eafd6fbaec2
VSRDVV <74640120+VSRDVV@users.noreply.github.com>
2021-01-19 21:26:27 +0700

    Add files via upload

---
+++ new.js
@@ -0,0 +1,37 @@
+//create a request variable
+var request = new XMLHttpRequest();
+//opne 
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+request.send();
+request.onload = function(){
+    var countrydata = JSON.parse(this.response);
+    var temp =[];
+    var la;
+    var lo;
+   for(var i = 0;i<countrydata.length;i++)
+   {
+        temp = countrydata[i].latlng;
+        la = temp[0];
+        lon = temp[1];
+       // console.log(lat,lon);
+       
+        var req = new XMLHttpRequest();
+        req.open('GET','http://api.openweathermap.org/data/2.5/weather?lat=%la%lon=&lo&appid=06d1ca1ab9695828efbfb2050ed9dad6',true);
+        req.send();
+        req.onload = function(){
+            var weatherdata = JSON.parse(this.response);
+            console.log(req);
+            
+        };//end of function
+                
+            }//end of for
+        
+        
+        }
+   //end of for
+   
+
+
+
+
+    
